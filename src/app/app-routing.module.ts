e755ee03c4a08b46e6e324eaccf52d09e1a15ed3
Gérald Devreux <gerald.devreux.pro@gmail.com>
2022-03-03 20:31:58 +0100

    User route path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomepageComponent} from "./components/homepage/homepage.component";
+import {UserComponent} from "./components/user/user.component";
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent }
+  { path: '', component: HomepageComponent },
+  { path: 'user', component: UserComponent }
 ];
 
 @NgModule({
