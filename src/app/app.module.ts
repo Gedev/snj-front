86dc1bb173e3be8e38d1ba49020c16bf9cd79965
Gérald Devreux <gerald.devreux.pro@gmail.com>
2022-03-03 15:27:02 +0100

    Add Student Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
