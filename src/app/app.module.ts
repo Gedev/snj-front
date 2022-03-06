04b0d0b64b9ea16e3e5325f5fd0eb59cc1ac0f71
SamiMas24 <samirahmasimina@gmail.com>
2022-03-06 13:55:18 +0100

    Front-end categorie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { UserComponent } from './components/user/user.component';
 import {HttpClientModule} from "@angular/common/http";
+import { CategorieComponent } from './components/categorie/categorie.component';
+import { AddCategorieComponent } from './components/add-categorie/add-categorie.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    UserComponent
+    UserComponent,
+    CategorieComponent,
+    AddCategorieComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        FontAwesomeModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
