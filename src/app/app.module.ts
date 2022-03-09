4495f57cf39e08f8128640c44cfb77a1b1f4b117
Gérald Devreux <gerald.devreux.pro@gmail.com>
2022-03-09 23:53:12 +0100

    Add an Angular material menu, add nav background color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import {CategorieComponent} from "./components/categorie/categorie.component";
 import {AddCategorieComponent} from "./components/add-categorie/add-categorie.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -24,14 +27,17 @@ import { MainNavigationComponent } from './components/main-navigation/main-navig
     AddCategorieComponent,
     MainNavigationComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        FontAwesomeModule,
-        BrowserAnimationsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
