80fe79357208675f1f96d530c4fcfb2e970e3918
pngabo <tipspaci@gmail.com>
2022-03-15 23:50:22 +0100

    Donation getOne, delete

--- src/app/components/donation/donation.component.ts
+++ src/app/components/donation/donation.component.ts
@@ -3,6 +3,7 @@ import {DonationService} from "../../services/donation.service";
 import {User} from "../../models/user";
 import {Address} from "../../models/address";
 import {Donation} from "../../models/donation";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-donation',
@@ -32,12 +33,18 @@ export class DonationComponent implements OnInit {
       lastname:""
     }
   };
+  selectedDon: number = -1;
+  donFormGroup: FormGroup;
+  onlyOne: boolean= false;
 
-  constructor(private dServ: DonationService) {
+  constructor(builder: FormBuilder, private dServ: DonationService) {
+    this.donFormGroup = builder.group({
+      'idDonation': new FormControl(null, [Validators.required])
+    });
   }
 
   ngOnInit(): void {
-    this.dServ.postUser({
+    this.dServ.postDonation({
         id: Date.now().toString().trim(),
         title: "Donation depuis front",
         hasCategory: false,
@@ -60,15 +67,52 @@ export class DonationComponent implements OnInit {
     ).subscribe({
       next: donation => console.log("Added donation: "+JSON.stringify(donation)),
       error: tempError => alert("Failed to add Donation in DB"),
-      complete: () => console.log("Success")
+      complete: () => console.log("Add one Succeded")
     });
   }
 
-  getOne() {
+  getAll() {
+    this.onlyOne = false;
+    this.found.id = "";
     this.dServ.getAll().subscribe({
       next: donations => this.listDonation = donations,
       error: tempError => alert("Failed to get all Donations from DB"),
       complete: () => console.log("getAll Succeded")
     })
+  }
+
+  getById() {
+    this.onlyOne = true;
+    let v = this.donFormGroup.value;
+    console.log("Click on RECHERCHE: ID = "+v.idDonation);
+    this.dServ.getDonationById(v.idDonation).subscribe({
+      next: donation => {
+        console.log("Found donation: "+JSON.stringify(donation));
+        this.found = donation;
+      },
+      error: tempError => alert("Failed to find Donation in DB"),
+      complete: () => console.log("getById Succeded")
+    });
+  }
+
+  selectedIndex(i: number) {
+    this.selectedDon = i;
+  }
+
+  deleteDonation() {
+    if(this.selectedDon != -1){
+      this.dServ.delete(this.listDonation[this.selectedDon].id).subscribe({
+        next: donation => {
+          console.log("Deleted donation: "+JSON.stringify(donation));
+          const v = this.listDonation[this.selectedDon];
+          this.listDonation.forEach( (item, index) => {
+            if(item === v)
+              this.listDonation.splice(index,1);
+          });
+        },
+        error: tempError => alert("Failed to delete Donation from DB"),
+        complete: () => console.log("Deleted Succeded")
+      });
+    }
   }
 }
--- src/app/services/donation.service.ts
+++ src/app/services/donation.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {DonationForm} from "../models/donation-form";
 import {Observable} from "rxjs";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Donation} from "../models/donation";
 
 @Injectable({
@@ -13,11 +13,23 @@ export class DonationService {
 
   constructor(private _donation: HttpClient) { }
 
-  public postUser(toPost: DonationForm): Observable<DonationForm> {
+  public postDonation(toPost: DonationForm): Observable<DonationForm> {
     return this._donation.post(this._apiUrl, toPost) as Observable<DonationForm>;
   }
 
   public getAll() : Observable<[Donation]> {
     return this._donation.get(this._apiUrl) as Observable<[Donation]>;
   }
+
+  public getDonationById(idDon: string) : Observable<Donation> {
+    const params = new HttpParams().set("id", idDon);
+    return this._donation.get(this._apiUrl,{params}) as Observable<Donation>;
+  }
+
+  public delete(toDelete: string) : Observable<DonationForm> {
+    console.log("ON VA SUPPRIMER l'id => "+toDelete);
+
+    return this._donation.delete(this._apiUrl+"/"+toDelete) as Observable<Donation>;
+  }
+
 }
